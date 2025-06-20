8be97b87932b70e0fc3aa7d2134917d8082905f7
vishwas-vadavi <vishwas.vadavi@network.lilly.com>
2025-06-20 12:18:41 +0530

    Update generator.py

--- kedro_sagemaker/generator.py
+++ kedro_sagemaker/generator.py
@@ -375,6 +375,7 @@ class KedroSageMakerGenerator:
         sm_node_name,
         sm_param_envs,
         sm_training_steps_with_model_outputs,
+        tags=None,
     ):
         return TrainingStep(
             sm_node_name,
